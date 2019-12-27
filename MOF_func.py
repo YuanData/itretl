9c31794ef0fd0a39f9e9b847c1422e23e330a7cd
Alan <kcykey@gmail.com>
2019-12-27 15:41:59 +0800

    重構 用dic_reports_version_func

--- MOF_func.py
+++ MOF_func.py
@@ -16,14 +16,7 @@ def gen_df_hs8_from_hs11_gpby(df_hs11):
     return df_hs8
 
 
-def gen_df_all_iy_regex():
-    iy_hs_mapping_str = r'//{dstore_ip}/dstore/重要資料/產業hscode對照表.xlsx'
-    # iy_hs_mapping_str = r'//{dstore_ip}/dstore/重要資料/產業hscode對照表_20191015.xlsx'
-    iy_hs_mapping_xlsx = iy_hs_mapping_str.format(dstore_ip=dstore_ip)
-    df_all_iy_xlsx = pd.read_excel(iy_hs_mapping_xlsx, 'sheet1')
-    # # df_all_iy_xlsx = pd.read_excel(r'SourceMaterial\產業hscode對照表.xlsx', 'sheet1')
-    # # df_all_iy_xlsx = pd.read_excel(r'SourceMaterial\產業hscode對照表.xlsx', '重要資料版_節選16')
-
+def gen_df_all_iy_by_xlsx_reports_version_1(df_all_iy_xlsx):
     # # 測試用
     # df_all_iy_xlsx = df_all_iy_xlsx[
     #     (df_all_iy_xlsx['大項'] == '16_機械及電機設備')
@@ -36,7 +29,7 @@ def gen_df_all_iy_regex():
         |
         (((df_all_iy_xlsx['大項'] == '遊艇') | (df_all_iy_xlsx['大項'] == '航太'))
          & (df_all_iy_xlsx['細項'] == '全部產品'))
-        ]
+        ].copy()
     # df_all_iy_xlsx = df_all_iy_xlsx[
     #     df_all_iy_xlsx['industry'] != '19_其他_傢俱'
     #     ]
@@ -57,40 +50,32 @@ def gen_df_all_iy_regex():
         df_all_iy_xlsx['選擇方式'] == '財政部定義產業',
         df_all_iy_xlsx['industry'].str.replace(re.compile(r'\d\d_'), ''),
         df_all_iy_xlsx['industry'])
-
-    df_all_iy_regex = pd.DataFrame()
-    for index, row in df_all_iy_xlsx.iterrows():
-        lst_1 = row['hscode'].split(',')
-        lst_2 = [s if len(s) <= 8 else s[:8] for s in lst_1]
-        lst_3 = ['^' + s for s in lst_2]
-        str_hs = '|'.join(lst_3)
-
-        df = pd.DataFrame.from_dict({'選擇方式': row['選擇方式'],
-                                     'industry': row['industry'],
-                                     'hscode': str_hs, }, orient='index').T
-        df_all_iy_regex = pd.concat([df_all_iy_regex, df])
-    df_all_iy_regex.reset_index(drop=True, inplace=True)
-    return df_all_iy_regex
+    return df_all_iy_xlsx
 
 
-def gen_df_all_iy_regex_reports_version_industry21():
+def gen_df_all_iy_xlsx_by_reports_version_2(df_all_iy_xlsx):
+    df = df_all_iy_xlsx[(df_all_iy_xlsx['reports_version_2'] == 1)].copy()
+    df.sort_values(['reports_version_2_order'], ascending=[True], inplace=True)
+    df.rename(columns={'industry': 'industry_original', 'reports_version_2_ind_name': 'industry'}, inplace=True)
+    return df
+
+
+dic_reports_version_func = {
+    'reports_version_1': gen_df_all_iy_by_xlsx_reports_version_1,
+    'reports_version_2': gen_df_all_iy_xlsx_by_reports_version_2,
+}
+
+
+def gen_df_all_iy_regex(reports_version='reports_version_1'):
     iy_hs_mapping_str = r'//{dstore_ip}/dstore/重要資料/產業hscode對照表.xlsx'
+    # iy_hs_mapping_str = r'//{dstore_ip}/dstore/重要資料/產業hscode對照表_20191015.xlsx'
     iy_hs_mapping_xlsx = iy_hs_mapping_str.format(dstore_ip=dstore_ip)
     df_all_iy_xlsx = pd.read_excel(iy_hs_mapping_xlsx, 'sheet1')
+    # # df_all_iy_xlsx = pd.read_excel(r'SourceMaterial\產業hscode對照表.xlsx', 'sheet1')
+    # # df_all_iy_xlsx = pd.read_excel(r'SourceMaterial\產業hscode對照表.xlsx', '重要資料版_節選16')
 
-    # 正式用
-    df_all_iy_xlsx = df_all_iy_xlsx[
-        (df_all_iy_xlsx['reports_version_industry21'] == 1)
-    ]
-    df_all_iy_xlsx.sort_values(['reports_version_industry21_order'], ascending=[True], inplace=True)
-    df_all_iy_xlsx = df_all_iy_xlsx[['選擇方式', 'industry', 'hscode']]
-
-    # df_all_iy_xlsx['industry'] = df_all_iy_xlsx['industry'].str.replace('_全部產品', '')
-
-    df_all_iy_xlsx['industry'] = np.where(
-        df_all_iy_xlsx['選擇方式'] == '財政部定義產業',
-        df_all_iy_xlsx['industry'].str.replace(re.compile(r'\d\d_'), ''),
-        df_all_iy_xlsx['industry'])
+    reports_version_func = dic_reports_version_func[reports_version]
+    df_all_iy_xlsx = reports_version_func(df_all_iy_xlsx)
 
     df_all_iy_regex = pd.DataFrame()
     for index, row in df_all_iy_xlsx.iterrows():
