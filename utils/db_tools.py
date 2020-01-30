a57e2b0af2a978669e11f0a99263ff1539fe1ae4
Alan <kcykey@gmail.com>
2020-01-30 14:29:40 +0800

    新增：def gen_itr_engine(db):

--- utils/db_tools.py
+++ utils/db_tools.py
@@ -27,3 +27,14 @@ def gen_itr_conn(db):
         db=db,
     )
     return conn
+
+
+def gen_itr_engine(db):
+    engine_args = 'mysql://{user}:{password}@{host}/{db}?charset=utf8mb4'.format(
+        host=PROPERTIES['HOSTNAME'],
+        user=PROPERTIES['USERNAME'],
+        password=PROPERTIES['PASSWORD'],
+        db=db,
+    )
+    engine = create_engine(engine_args)
+    return engine
